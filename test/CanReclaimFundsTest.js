6e5148716e3918e3d7cb7fb6a5391ef3e9a4f8e6
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2020-04-17 17:36:30 -1000

    Update CanReclaimFundsTest.js

--- test/CanReclaimFundsTest.js
+++ test/CanReclaimFundsTest.js
@@ -1,21 +1,21 @@
-const PAXGMock = artifacts.require('PAXGWithBalance.sol');
+const GTSGMock = artifacts.require('GTSGWithBalance.sol');
 const Proxy = artifacts.require('AdminUpgradeabilityProxy.sol');
 
 const assertRevert = require('./helpers/assertRevert');
 
-// Test that the PAXG contract can reclaim PAXG it has received.
+// Test that the GTSG contract can reclaim GTSG it has received.
 // Note that the contract is not payable in Eth.
 contract('CanReclaimFunds', function ([_, admin, owner, assetProtectionRole, anyone]) {
   const amount = 100;
 
   beforeEach(async function () {
     // Create contract and token
-    const paxg = await PAXGMock.new({from: owner});
+    const paxg = await GTSGMock.new({from: owner});
     const proxy = await Proxy.new(paxg.address, {from: admin});
-    const proxiedPAXG = await PAXGMock.at(proxy.address);
-    await proxiedPAXG.initialize({from: owner});
-    await proxiedPAXG.initializeBalance(owner, amount);
-    this.token = proxiedPAXG;
+    const proxiedGTSG = await GTSGMock.at(proxy.address);
+    await proxiedGTSG.initialize({from: owner});
+    await proxiedGTSG.initializeBalance(owner, amount);
+    this.token = proxiedGTSG;
 
     // Send token to the contract
     await this.token.transfer(this.token.address, amount, {from: owner});
@@ -36,7 +36,7 @@ contract('CanReclaimFunds', function ([_, admin, owner, assetProtectionRole, any
   });
 
   it('should allow owner to reclaim tokens', async function () {
-    await this.token.reclaimPAXG({from: owner});
+    await this.token.reclaimGTSG({from: owner});
 
     const balance = await this.token.balanceOf(owner);
     assert.equal(amount, balance);
@@ -46,7 +46,7 @@ contract('CanReclaimFunds', function ([_, admin, owner, assetProtectionRole, any
 
   it('should allow only owner to reclaim tokens', async function () {
     await assertRevert(
-      this.token.reclaimPAXG({from: anyone})
+      this.token.reclaimGTSG({from: anyone})
     );
   });
 });
