4051bc3e5cf6870ed95208ecebb967374bc12ce1
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2020-04-17 17:33:07 -1000

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
-const PAXG = artifacts.require('PAXGImplementation');
+const PAXG = artifacts.require('GTSGImplementation');
 const Proxy = artifacts.require('AdminUpgradeabilityProxy');
 
 module.exports = async function(deployer) {
   await deployer;
 
-  await deployer.deploy(PAXG);
-  const proxy = await deployer.deploy(Proxy, PAXG.address);
+  await deployer.deploy(GTSG);
+  const proxy = await deployer.deploy(Proxy, GTSG.address);
   const proxiedPAXG = await PAXG.at(proxy.address);
-  await proxy.changeAdmin("0xf0b1eef88956b0a307fa87b5f5671aad6a5d330f");
-  await proxiedPAXG.initialize();
+  await proxy.changeAdmin("<Add Admin wallet address>");
+  await proxiedGTSG.initialize();
 };
